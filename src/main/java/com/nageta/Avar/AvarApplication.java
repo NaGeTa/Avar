73ae4ad5df9e12516cabbdbe13056311a65c26e4
NaGeTa <dagestan200221@mail.ru>
2025-07-27 20:02:03 +0300

    initial commit

---
+++ src/main/java/com/nageta/Avar/AvarApplication.java
@@ -0,0 +1,13 @@
+package com.nageta.Avar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nageta/Avar/AvarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nageta.Avar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
